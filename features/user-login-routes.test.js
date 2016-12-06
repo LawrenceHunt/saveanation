939dee8640762f0a2da3907bb095120e3fe74592
Frances Maxwell <fmaxwell@gmail.com>
2016-12-06 15:49:57 +0000

    Failing test for user sign-up

--- features/user-login-routes.test.js
+++ features/user-login-routes.test.js
@@ -1,30 +1,27 @@
 import { signUp, signIn, getBrowser, cleanDatabase } from './testHelpers.test'
 
-describe("User Login", function () {
+describe("User SignUp", function () {
 
-  beforeEach(function(){
-    server.call('logout');
-    cleanDatabase();
-  });
+  // beforeEach(function(){
+  //   server.call('logout');
+  //   cleanDatabase();
+  // });
 
   describe("New user", function () {
-    it("gets redirected to new character form on sign up", function () {
-      var browserInstance = getBrowser(0)
-      signUp('Pikachu@pika.com', 'pikapika')
-      signIn(browserInstance, 'Pikachu@pika.com', 'pikapika');
-      browserInstance.waitForExist("form.newCharacterForm", 2000);
-      expect(browserInstance.getUrl()).to.equal('http://localhost:3000/character/new');
+    it("allows a new user to sign up", function () {
+      signUp('pika', 'Pika', 'Chu', 'Pikachu@pika.com', 'pikapika')
+      expect().to.equal();
     });
   });
 
-  describe("Signed out user", function () {
-    it("redirects to home page if trying to access /character/new", function () {
-      var browserInstance = getBrowser(0)
-      signUp('Pikachu@pika.com', 'pikapika')
-      browserInstance.url('http://localhost:3000/character/new')
-      .waitForExist('div.billboard', 2000);
-      expect(browserInstance.getUrl()).to.equal('http://localhost:3000/');
-    });
-  });
+  // describe("Signed out user", function () {
+  //   it("redirects to home page if trying to access /character/new", function () {
+  //     var browserInstance = getBrowser(0)
+  //     signUp('Pikachu@pika.com', 'pikapika')
+  //     browserInstance.url('http://localhost:3000/character/new')
+  //     .waitForExist('div.billboard', 2000);
+  //     expect(browserInstance.getUrl()).to.equal('http://localhost:3000/');
+  //   });
+  // });
 
 });
