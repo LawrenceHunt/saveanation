72dfece5b63595d8389ef3750bc2efc8bdd34182
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-12-07 11:35:05 +0000

    test written for match error

--- imports/api/deposits/deposits.tests.js
+++ imports/api/deposits/deposits.tests.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
+import { expect } from 'meteor/practicalmeteor:chai';
 import { assert } from 'meteor/practicalmeteor:chai';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
 
@@ -8,15 +9,18 @@ import { Deposits } from './deposits.js';
 if(Meteor.isServer) {
   describe('Deposits', function() {
     describe('methods', function() {
-
+      const addDeposit = Meteor.server.method_handlers['deposits.add'];
       beforeEach(function(){
         resetDatabase();
       });
 
       it('can add a deposit', function(){
-        const addDeposit = Meteor.server.method_handlers['deposits.add'];
         addDeposit.call(this,125,"Test");
-        assert.equal(Deposits.find({}).count(), 1);
+        expect(Deposits.find({}).count()).to.equal(1);
+      });
+      it("can't put random shit in", function(){
+        // expect(addDeposit.call(this, "fifty thirteen bloody quid", "mate")).to.throw(Error);
+        assert.throws(function() {addDeposit.call(this, "fifty thirteen bloody quid", "mate"); }, /Match error/);
       });
     });
   });
