f99e869e1824d74e40179fe66be5dd7e678e50af
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-12-08 22:38:33 +0000

    added slider to homepage

--- imports/ui/layouts/homeLayout.js
+++ imports/ui/layouts/homeLayout.js
@@ -2,3 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import './homeLayout.html';
 import '../styles/clouds.css';
+import { slick } from 'meteor/udondan:slick';
+
+if (Meteor.isClient) {
+  Template.carousel.rendered = function() {
+    $('#carousel').slick({
+      dots: true,
+      arrows: true
+    });
+  };
+}
