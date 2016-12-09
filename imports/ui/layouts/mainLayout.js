15a6aebb03dfbcf2f82cb1bc5b9699c6a2e811c5
hanrattyjen <hanratty.jen@gmail.com>
2016-12-09 14:23:41 +0000

    Tidied up nav for currentUser.
    Added logout functionatlity for a sign in user.

--- imports/ui/layouts/mainLayout.js
+++ imports/ui/layouts/mainLayout.js
@@ -6,3 +6,11 @@ import '../feed/feed.js';
 import '../save/save.js';
 import '../user/login.js';
 import '../user/register.js';
+
+Template.Dashboard.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+        FlowRouter.go('/');
+    }
+});
