5b9b62923f2a102e576ffd5493d135d7e040da29
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-12-12 22:58:19 +0000

    a few methods for the game now tried. Next trying images

--- imports/ui/tower/tower.js
+++ imports/ui/tower/tower.js
@@ -9,14 +9,14 @@ import './tower.html';
 
 
 // when user first connects
-Template.Tower.onCreated(function(){
-
-});
-
-Template.Tower.helpers({
-
-});
+// Template.Tower.onCreated(function(){
 //
+// });
+//
+// Template.Tower.helpers({
+//
+// });
+// BASIC DRAWING METHODS
 // var updateTowerCanvas = function(gamecanvas) {
 //   Tracker.autorun(function(){
 //     let ctx = gamecanvas.getContext('2d');
@@ -32,52 +32,33 @@ Template.Tower.helpers({
 //   updateTowerCanvas(gamecanvas);
 // });
 //
-// Template.Tower.events({
-//   '.click #block'(){
+
+
+// // RAPHAEL METHODS
+// Template.Tower.onRendered(function(){
+//   // var paper = Raphael("canvas", "100%", "100%");
+//   // createBlock();
+//   // createTetromino();
+// });
 //
+//
+// var createBlock = function(paper) {
+//   var circle = paper.circle("50%", "50%", 40);
+// };
+//
+// var createTetromino = function(paper) {
+//   var tetronimo = paper.path("M 250 250 l 0 -50 l -50 0 l 0 -50 l -50 0 l 0 50 l -50 0 l 0 50 z");
+//    tetronimo.attr({fill: '#9cf', stroke: '#ddd', 'stroke-width': 5});
+// };
+//
+// Template.Tower.events({
+//   'click #brick-item': function(event){
+//     var paper = Raphael("canvas", "100%", "100%");
+//     createBoth(paper);
 //   }
 // });
-
-
-Template.Tower.onRendered(function(){
-  paper = Raphael("paper", "100%", "100%");
-<<<<<<< HEAD
-=======
-  
->>>>>>> master
-  var t = paper.text(50, 10, "HTML5ROCKS");
-
-  var letters = paper.print(50, 50, "HTML5ROCKS", paper.getFont("Vegur"), 40);
-
-  letters[4].attr({fill:"orange"});
-  for (var i = 5; i < letters.length; i++) {
-    letters[i].attr({fill: "#3D5C9D", "stroke-width": "2", stroke: "#3D5C9D"});
-  }
-});
-
-var createGoban = function () {
-  // Create goban
-  // Based off of svg in public domain: http://commons.wikimedia.org/wiki/File:Blank_Go_board.svg
-  // Converted using http://readysetraphael.com/
-  // var rect_a = paper.rect(0, 0, 96, 96);
-  // rect_a.attr({fill: '#DCB35C','stroke-width': '0','stroke-opacity': '1'}).data('id', 'rect_a');
-  // var path_b = paper.path("M2.9,93h90.2m-.2-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m0-5h90m0-5H3m-.1-5h90.2M3,3V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3m5,0V93m5,0V3");
-  // path_b.attr({stroke: '#000',"stroke-width": '0.2',fill: 'none','stroke-opacity': '1'}).data('id', 'path_b');
-  // var path_c = paper.path("M18,78l0,0m30,0l0,0m30,0l0,0m0-30l0,0m-30,0l0,0m-30,0l0,0m0-30l0,0m30,0l0,0m30,0l0,0");
-  // path_c.attr({stroke: '#000',"stroke-width": '4',"stroke-linecap": 'round','stroke-opacity': '1','fill': '#000000'}).data('id', 'path_c');
-  // var goban = [rect_a, path_b, path_c];
-  //
-  // // Scale goban up since rsr outputs reduced size
-  // for(var i = 0; i < goban.length; i++) {
-  //   goban[i].transform('S5,5,0,0');
-  // }
-  var paper = Raphael("sample-4", 600, 100);
-var t = paper.text(50, 10, "HTML5ROCKS");
-
-var letters = paper.print(50, 50, "HTML5ROCKS", paper.getFont("Vegur"), 40);
-
-letters[4].attr({fill:"orange"});
-for (var i = 5; i < letters.length; i++) {
-  letters[i].attr({fill: "#3D5C9D", "stroke-width": "2", stroke: "#3D5C9D"});
-}
-};
+//
+// function createBoth(paper){
+//   createTetromino(paper); createBlock(paper);
+// }
+//
