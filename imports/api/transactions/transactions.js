de35de0f75f4c161e7bb211ee5059fcf82c244a2
Frances Maxwell <fmaxwell@gmail.com>
2016-12-14 23:19:19 +0000

    Wrote test for decimal savings. Fixed HTML form and JS parsing.

--- imports/api/transactions/transactions.js
+++ imports/api/transactions/transactions.js
@@ -6,28 +6,24 @@ import { SavingsAccounts } from '../savingsAccounts/savingsAccounts.js';
 export const Transactions = new Mongo.Collection('transactions');
 
 if(Meteor.isServer) {
-  // This code only runs on the server
-  // Only publish transactions that belong to the current user
   Meteor.publish('transactions', function transactionsPublication() {
     return Transactions.find({
-      // Publish only the current user's transactions! Bring back once User ID is in place.
        owner: this.userId
     });
   });
 }
-
 
 Meteor.methods({
   'transactions.add'(amount, text, type){
     check(amount, Number);
     check(text, String);
     check(type, String);
-    // Checks user is logged in - bring back once User ID is in place.
+
     if(!this.userId) {
       throw new Meteor.Error('not-authorized');
     }
     var currentUserId = this.userId;
-    // Create the transaction object
+
     Transactions.insert({
       amount: amount,
       owner: currentUserId,
@@ -35,7 +31,7 @@ Meteor.methods({
       createdAt: new Date(),
       type: type
     });
-    // Adjust balance
+
     Meteor.call('savingsAccounts.adjustBalance', amount, currentUserId);
   }
 });
--- imports/ui/save/save.js
+++ imports/ui/save/save.js
@@ -43,7 +43,7 @@ Template.Save.events({
   'submit .add-deposit'(event) {
     event.preventDefault();
     const target = event.target;
-    const amount = parseInt(target.amount.value);
+    const amount = parseFloat(target.amount.value);
     const text = target.text.value;
     if (noAccount()) {
       Meteor.call('savingsAccounts.create');
