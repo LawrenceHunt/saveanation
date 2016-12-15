0aefa69e40a163df4bd1147d3857a3eb5c6927f3
Frances Maxwell <fmaxwell@gmail.com>
2016-12-15 13:57:34 +0000

    Badge icons uploaded. Badge templates created.

---
+++ imports/api/badges/badges.tests.js
@@ -0,0 +1,27 @@
+// import { Meteor } from 'meteor/meteor';
+// import { Random } from 'meteor/random';
+// import { assert } from 'meteor/practicalmeteor:chai';
+// import { resetDatabase } from 'meteor/xolvio:cleaner';
+// import { Transactions } from './transactions.js';
+// import { SavingsAccounts } from '../savingsAccounts/savingsAccounts.js';
+//
+// if(Meteor.isServer) {
+//   describe('Badges', function() {
+//     describe('methods', function() {
+//       const userId = Random.id();
+//       const createdAt = new Date();
+//       const invocation = { userId };
+//
+//       beforeEach(function(){
+//         resetDatabase();
+//       });
+//
+//       it('awards a badge if you save twice in a day', function(){
+//
+//       });
+//       it('awards a badge if you invite a team member', function(){
+//
+//       });
+//     });
+//   });
+// };
